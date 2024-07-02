e1069454310b45c8935ccd1d6dd65041991d032e
Anaghakuzikktil <108046922+Anaghakuzikktil@users.noreply.github.com>
2024-07-02 14:21:51 +0530

    Create brandname.py

---
+++ brandname.py
@@ -0,0 +1,4 @@
+print("Welcome to the brand name generator")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name= input("What's your pet's name?\n")
+print("your brand name could be " +city_name + " " + pet_name)
